45e6f76eb1a53403ec489743b16b12c52bd95cc0
Pierre <pierre.massonie@etu.unilim.fr>
2023-06-22 11:35:39 +0200

    [Elements] No overload in C ><.

--- source/StaticBlock.c
+++ source/StaticBlock.c
@@ -1,13 +1,6 @@
 #include "StaticBlock.h"
 
 
-StaticBlock* newStaticBlock(){
-    StaticBlock* block = malloc(sizeof(StaticBlock));
-
-    block->hasColision = false;
-
-    return block;
-}
 
 StaticBlock* newStaticBlock(bool colision){
     StaticBlock* block = malloc(sizeof(StaticBlock));
