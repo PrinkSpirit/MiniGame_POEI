9a1b9b2af3d0fa3fb9cb88c0f0c793b606a4decb
Pierre <pierre.massonie@etu.unilim.fr>
2023-06-22 11:15:41 +0200

    [Elements] Added staticBlocks

---
+++ header/StaticBlock.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "GameElement.h"
+
+/** StaticBlock
+ * Define elements that will never move and have no interaction other than collision.
+ * Some will have collision, others won't.
+ * 
+ * Eg. Ground, Wall, Cloud, Tree, ect.
+*/
+typedef struct StaticBlock{
+    GameElement* element;
+
+    // Define if actors can colide with it
+    bool hasColision;
+
+}StaticBlock;
