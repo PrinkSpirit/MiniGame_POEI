aaa9408603f5169f9904bcd4568fc62c8d4bfcd5
Pierre <pierre.massonie@etu.unilim.fr>
2023-06-22 11:15:21 +0200

    [Elements] Added comments

--- header/GameElement.h
+++ header/GameElement.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <stdlib.h>
 
+/** GameElememt
+ * Basis for other elements that will be displayed ingame
+ * This should only be used inside another element
+*/
 typedef struct GameElement{
     // Position and size from the bottom left
     float pos_x;
